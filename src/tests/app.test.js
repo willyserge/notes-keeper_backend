00105b6352b9cfb44e08d6f097d3853b156618ca
willyserge <willyserge01@gmail.com>
2020-03-27 14:11:22 +0200

    add tests for the sign up endpoint

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -1,14 +1,45 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-undef */
 import supertest from 'supertest';
+import mongoose from 'mongoose';
 import app from '../app';
+import User from '../models/user';
+import Db from './dbOperations';
+
 
 const request = supertest(app);
 
-describe('test endpoint', () => {
-  it('gets the test endpoint', async (done) => {
-    const response = await request.get('/test');
 
-    expect(response.status).toBe(200);
+beforeAll(async () => {
+  mongoose.connect(process.env.TEST_DB,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+});
 
+afterEach(async () => {
+  Db.removeAllCollections();
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
+
+describe('POST /api/auth/signup', () => {
+  it('should register a new user', async (done) => {
+    const newUser = {
+      firstname: 'willy',
+      lastname: 'test',
+      email: 'testingemail@gmail.com',
+      password: 'testPass123'
+    };
+    const res = await request.post('/api/auth/signup')
+      .send(newUser).expect(201);
+    const user = await User.findById(res.body.user._id);
+    expect(user).not.toBeNull();
+    expect(user.email).toBeTruthy();
+    expect(user.password).not.toBe('testPass123');
     done();
   });
 });
