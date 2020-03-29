752be9c7cb873c0814b494b1379aef02315d6b84
willyserge <willyserge01@gmail.com>
2020-03-29 15:32:56 +0200

    create authorization middleware

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ import 'regenerator-runtime/runtime';
 import express from 'express';
 import createError from 'http-errors';
 import authRouter from './routes/auth';
+import notesRouter from './routes/notes';
+import auth from './middleware/auth';
 
 const app = express();
 
 app.use(express.json());
 app.use('/api/auth', authRouter);
+app.use('/api', auth, notesRouter);
 
 app.use((req, res, next) => {
   next(createError(404));
